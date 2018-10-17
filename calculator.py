c4aa7cd6ff5af926ed0ac535627e87debc3a3a38
zxsz666 <zxsz66@163.com>
2018-10-17 21:08:00 +0800

    add calculator.py

---
+++ calculator.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import sys
+
+tax = int(sys.argv[1])- 3500
+
+if tax <= 1500:
+    fee = tax * 0.03 - 0.00
+    print(format(fee,".2f")
+
+if tax > 1500 and tax <= 4500:
+    fee = tax * 0.10 - 105
+    print(format(fee,".2f"))
+
+if tax > 4500 and tax <= 9000:
+    fee = tax * 0.2 - 555
+    print(format(fee,".2f"))
+
+if tax > 9000 and tax <= 35000:
+    fee = tax * 0.25 - 1005
+    print(format(fee,".2f"))
+
+if tax > 35000 and tax <= 55000:
+    fee = tax * 0.3 - 2755
+    print(format(fee,".2f"))
+
+if tax > 55000 and tax <= 80000:
+    fee = tax * 0.35 - 5505
+    print(format(fee,".2f"))
+
+if tax > 80000:
+    fee = tax * 0.45 - 13505
+    print(format(fee,".2f"))
